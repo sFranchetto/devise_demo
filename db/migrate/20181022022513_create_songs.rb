e65b11dbe05f25c6caa7753a97785337bca5a1df
Steven Franchetto <steven.franchetto@gmail.com>
2018-10-21 23:06:36 -0400

    Commit message

---
+++ db/migrate/20181022022513_create_songs.rb
@@ -0,0 +1,11 @@
+class CreateSongs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :songs do |t|
+      t.string :title
+      t.text :description
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
